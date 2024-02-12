fb034afe02271dd50dba712544fab4391aae847d
Baganna Kumbar <bagannak@testvagrant.com>
2024-02-12 17:11:23 +0530

    AnonymousInnerClass added

---
+++ src/main/java/com/learn/java8features/AnonymousInnerClassDemo2.java
@@ -0,0 +1,21 @@
+package com.learn.java8features;
+
+public class AnonymousInnerClassDemo2 {
+
+    public static void main(String[] args) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Child thread");
+                }
+            }
+        };
+        Thread t = new Thread(runnable);
+        t.start();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Main Thread");
+        }
+    }
+
+}
